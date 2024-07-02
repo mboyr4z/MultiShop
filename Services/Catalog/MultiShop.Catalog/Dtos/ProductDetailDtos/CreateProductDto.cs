a4a82c36483c0fa2f8a26880327be41b0a214ea0
Muhammet boyraz <82450697+mboyr4z@users.noreply.github.com>
2024-07-02 16:19:16 +0300

    Product and category dtos added

---
+++ Services/Catalog/MultiShop.Catalog/Dtos/ProductDetailDtos/CreateProductDto.cs
@@ -0,0 +1,11 @@
+﻿ namespace MultiShop.Catalog.Dtos.ProductDetailDtos
+{
+    public class CreateProductDto
+    {
+        public string ProductName { get; set; }
+        public decimal Price { get; set; }
+        public string ProductImageUrl { get; set; }
+        public string ProductDescription { get; set; }
+        public string CategoryId { get; set; }
+    }
+}
---
+++ Services/Catalog/MultiShop.Catalog/Dtos/ProductDetailDtos/ResultProductDto.cs
@@ -0,0 +1,12 @@
+﻿ namespace MultiShop.Catalog.Dtos.ProductDetailDtos
+{
+    public class ResultProductDto
+    {
+        public string ProductID { get; set; }
+        public string ProductName { get; set; }
+        public decimal Price { get; set; }
+        public string ProductImageUrl { get; set; }
+        public string ProductDescription { get; set; }
+        public string CategoryId { get; set; }
+    }
+}
