e79db577cb9f40987526aa92dedfaad52ab51ddf
Muhammet boyraz <82450697+mboyr4z@users.noreply.github.com>
2024-07-04 14:58:12 +0300

    Coupon dtos added

---
+++ Services/Discount/MultiShop.Discount/Dtos/ResultCouponDto.cs
@@ -0,0 +1,15 @@
+﻿namespace MultiShop.Discount.Dtos
+{
+    public class ResultCouponDto
+    {
+        public int CouponId { get; set; }
+
+        public string Code { get; set; }
+
+        public int Rate { get; set; }
+
+        public bool IsActive { get; set; }
+
+        public DateTime Validdate { get; set; }
+    }
+}
---
+++ Services/Discount/MultiShop.Discount/Services/DiscountService.cs
@@ -0,0 +1,9 @@
+﻿using MultiShop.Discount.Dtos;
+
+namespace MultiShop.Discount.Services
+{
+    public class DiscountService
+    {
+        
+    }
+}
