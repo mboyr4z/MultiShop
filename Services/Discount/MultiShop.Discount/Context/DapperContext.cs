20a3f3cb250d93630e27110e1069d0a83e41ab7b
Muhammet boyraz <82450697+mboyr4z@users.noreply.github.com>
2024-07-04 14:53:19 +0300

    Discount mssql connection is added

--- Services/Discount/MultiShop.Discount/Context/DapperContext.cs
+++ Services/Discount/MultiShop.Discount/Context/DapperContext.cs
@@ -1,8 +1,28 @@
-﻿using Microsoft.EntityFrameworkCore;
+﻿using Microsoft.Data.SqlClient;
+using Microsoft.EntityFrameworkCore;
+using MultiShop.Discount.Entities;
+using System.Data;
 
 namespace MultiShop.Discount.Context
 {
     public class DapperContext : DbContext
     {
+        private readonly IConfiguration _configuration;
+        private readonly string _connectionString;
+
+        public DapperContext(IConfiguration configuration)
+        {
+            _configuration = configuration;
+            _connectionString = _configuration.GetConnectionString("DefaultConnection");
+        }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer("Server=DESKTOP-9RMO5H8;initial Catalog=MultiShopDiscountDb;integrated Security=true;TrustServerCertificate=True;");    
+        }
+
+        public DbSet<Coupon>  Coupons{ get; set; }
+        public IDbConnection CreateConnection() => new SqlConnection(_connectionString);
+
     }
 }
--- Services/Discount/MultiShop.Discount/Entities/Coupon.cs
+++ Services/Discount/MultiShop.Discount/Entities/Coupon.cs
@@ -2,5 +2,15 @@
 {
     public class Coupon
     {
+        public int CouponId{ get; set; }
+
+        public string Code{ get; set; }
+
+        public int Rate { get; set; }
+
+        public bool IsActive { get; set; }
+
+        public DateTime Validdate { get; set; }
+
     }
 }
