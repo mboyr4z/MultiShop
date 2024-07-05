1f63eec1aad38f4404763e397b58b094622a3c81
Muhammet boyraz <82450697+mboyr4z@users.noreply.github.com>
2024-07-05 19:14:16 +0300

    CQRS Commands, Handlers, Queries, Results methods added for address

---
+++ Services/Order/Core/MultiShop.Order.Application/Features/CQRS/Handlers/AddressHandlers/CreateAddressCommandHandler.cs
@@ -0,0 +1,14 @@
+﻿using MultiShop.Order.Application.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MultiShop.Order.Application.Features.CQRS.Handlers.AddressHandlers
+{
+    public class CreateAddressCommandHandler
+    {
+        private readonly IRepository<Address> 
+    }
+}
