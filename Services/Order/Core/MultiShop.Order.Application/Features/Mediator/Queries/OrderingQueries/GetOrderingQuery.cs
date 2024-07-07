2c1118a58eaf171c428b365d3ea66ab5daddf8c5
Muhammet boyraz <82450697+mboyr4z@users.noreply.github.com>
2024-07-07 13:34:08 +0300

    Ordering comamnd and queries added for Mediator design

---
+++ Services/Order/Core/MultiShop.Order.Application/Features/Mediator/Queries/OrderingQueries/GetOrderingQuery.cs
@@ -0,0 +1,20 @@
+﻿using MediatR;
+using MultiShop.Order.Application.Features.Mediator.Results.OrderingResults;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MultiShop.Order.Application.Features.Mediator.Queries.OrderingQueries
+{
+    public class GetOrderingQuery : IRequest<List<GetOrderingQueryResult>>
+    {
+        public int Id { get; set; }
+
+        public GetOrderingQuery(int id)
+        {
+            Id = id;
+        }
+    }
+}
