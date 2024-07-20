d59ababdb761777691df0c46ee82f2aa4c84a6d0
Muhammet boyraz <82450697+mboyr4z@users.noreply.github.com>
2024-07-20 19:58:24 +0300

    Basket service methods completed

---
+++ Services/Basket/MultiShop.Basket/Services/BasketService.cs
@@ -0,0 +1,33 @@
+﻿using MultiShop.Basket.Dtos;
+using MultiShop.Basket.Settings;
+using System.Text.Json;
+
+namespace MultiShop.Basket.Services
+{
+    public class BasketService : IBasketService
+    {
+        private readonly RedisService _redisService;
+
+        public BasketService(RedisService redisService)
+        {
+            _redisService = redisService;
+        }
+
+        public async Task DeleteBasket(string userId)
+        {
+            _redisService.GetDb().KeyDeleteAsync(userId);
+        }
+
+
+        public async Task<BasketTotalDto> GetBasket(string userId)
+        {
+            var existBasket = await _redisService.GetDb().StringGetAsync(userId);
+            return JsonSerializer.Deserialize<BasketTotalDto>(existBasket);
+        }
+
+        public async Task SaveBasket(BasketTotalDto basketTotalDto)
+        {
+            _redisService.GetDb().StringSetAsync(basketTotalDto.UserId, JsonSerializer.Serialize(basketTotalDto));
+        }
+    }
+}
