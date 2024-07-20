7d55c30d00c01c4e3b98ce9ee47a94dcac4ef5b1
Muhammet boyraz <82450697+mboyr4z@users.noreply.github.com>
2024-07-20 13:34:38 +0300

    RepositoryManager business completed

---
+++ Services/Cargo/MultiShop.Cargo.BusinessLayer/Concrete/CargoOperationManager.cs
@@ -0,0 +1,44 @@
+﻿using MultiShop.Cargo.DataAccessLayer.Abstract;
+using MultiShop.Cargo.EntityLayer.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MultiShop.Cargo.BusinessLayer.Concrete
+{
+    public class CargoOperationManager : ICargoOperationDal
+    {
+        private readonly ICargoOperationDal _cargoOperationDal;
+
+        public CargoOperationManager(ICargoOperationDal cargoOperationDal)
+        {
+            _cargoOperationDal = cargoOperationDal;
+        }
+        public void Delete(int id)
+        {
+            _cargoOperationDal.Delete(id);
+        }
+
+        public List<CargoOperation> GetAll()
+        {
+            return _cargoOperationDal.GetAll();
+        }
+
+        public CargoOperation GetById(int id)
+        {
+            return _cargoOperationDal.GetById(id);
+        }
+
+        public void Insert(CargoOperation entity)
+        {
+            _cargoOperationDal.Insert(entity);
+        }
+
+        public void Update(CargoOperation entity)
+        {
+            _cargoOperationDal.Update(entity);
+        }
+    }
+}
