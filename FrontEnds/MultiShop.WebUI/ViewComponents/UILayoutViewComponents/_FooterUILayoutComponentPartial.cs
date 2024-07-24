9537d7d2801efd6a70e2e4ad21934286a4294626
Muhammet boyraz <82450697+mboyr4z@users.noreply.github.com>
2024-07-24 15:52:11 +0300

    Main Page design edit

---
+++ FrontEnds/MultiShop.WebUI/ViewComponents/UILayoutViewComponents/_FooterUILayoutComponentPartial.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace MultiShop.WebUI.ViewComponents.UILayoutViewComponents
+{
+   
+    public class _FooterUILayoutComponentPartial : ViewComponent
+    {
+        public IViewComponentResult Invoke()
+        {
+
+            return View();
+        }
+    }
+}
